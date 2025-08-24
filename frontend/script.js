b99202245445939db503e800f2b32ba8b6970dff
PROroblox1212 <alexischarlebois499@gmail.com>
2025-08-24 15:16:53 -0400

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,18 @@
+async function sendMessage() {
+  const input = document.getElementById("user-input");
+  const message = input.value;
+  input.value = "";
 
+  const chatBox = document.getElementById("chat-box");
+  chatBox.innerHTML += `<div class='user'><b>Toi:</b> ${message}</div>`;
+
+  const response = await fetch("http://127.0.0.1:8000/chat", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({message: message})
+  });
+  const data = await response.json();
+
+  chatBox.innerHTML += `<div class='ai'><b>IA:</b> ${data.reply}</div>`;
+  chatBox.scrollTop = chatBox.scrollHeight;
+}
